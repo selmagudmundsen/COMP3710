67353fc68928ed29ee2460750056fdfc886c4b09
Selma <selma.gudmundsen@gmail.com>
2023-08-02 08:57:44 +1000

    compute and plot gaussian

--- demo1.py
+++ demo1.py
@@ -2,3 +2,27 @@ import torch
 import numpy as np
 
 print("Pytorch version: ", torch.__version__)
+
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# grid for computing image, subdivide the space
+X, Y = np.grid[-4, 0:4:0.01, -4, 0:4:0.01]
+
+# load into PyTorch tensors
+x = torch.Tensor(X)
+y = torch.Tensor(Y)
+
+# transfer to the GPU device
+x = x.to(device)
+y = y.to(device)
+
+# Compute Gaussian function
+z = torch.exp(-(x**2 + y**2)/2.0)
+
+# plot
+import matplotlib.pyplot as plt
+
+plt.imshow(z.numpy())
+plt.tight_layout()
+plt.show()
